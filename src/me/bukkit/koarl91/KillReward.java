d193339028b341cbcde703f4c501753f2d275b74
BuildTools <unconfigured@null.spigotmc.org>
2015-03-10 21:33:56 +0100

    abc

---
+++ src/me/bukkit/koarl91/KillReward.java
@@ -0,0 +1,20 @@
+package me.bukkit.koarl91;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KillReward extends JavaPlugin {
+	
+	public static KillReward plugin;
+	
+	@Override
+	public void onEnable() {
+		this.getCommand("killreward").setExecutor(new KillRewardCommandHandler());
+		this.getCommand("killreward").setTabCompleter(new KillRewardCommandTabCompleter());
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
---
+++ src/me/bukkit/koarl91/KillRewardCommandHandler.java
@@ -0,0 +1,34 @@
+package me.bukkit.koarl91;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.HelpCommand;
+
+public class KillRewardCommandHandler implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		Command cmd = new HelpCommand(sender);
+		
+		//no args show help
+		if (args.length == 0) {
+			cmd.execute(sender, command, label, args);
+			return true;
+		}
+		
+		switch (args[0].toLowerCase()) {
+			case "info":
+			cmd = new InfoCommand(sender);
+			break;
+			case "help":
+			cmd = new HelpCommand(sender);
+			break;
+		}
+		
+		cmd.execute(sender, command, label, args);
+		return true;
+	}
+}
+
